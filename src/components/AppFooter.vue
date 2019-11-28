309fd5926c03fdf51e405161cf1bff36c2d89750
Gustavo Melki <contato@gustavomelki.com.br>
2019-11-28 15:18:04 -0200

    Add style to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,11 @@
 <template>
-  <footer class="container">
-    <span>© 2019 Grupo Boticário · Todos os direitos reservados</span>
+  <footer class="app-footer container">
+    <small>© 2019 Grupo Boticário · Todos os direitos reservados</small>
   </footer>
 </template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
