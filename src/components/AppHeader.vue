10dfeae9f6b719555e5b0328141467ef7a1cd927
Gustavo Melki <contato@gustavomelki.com.br>
2019-11-28 23:10:39 -0200

    Finalizing header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,17 +6,22 @@
         alt="Ícone Grupo Boticário"
         class="app-header__icon"
       />
-      <nav-header />
-      <div>
-        <span>Cashback acumulado</span>
-        <span>R$ 900,00</span>
-      </div>
-      <div>
-        <div>
-          <span>Olá, Gustavo</span>
-          <button title="Sair">Sair</button>
+      <div class="app-header__collapse">
+        <nav-header class="app-header__nav" />
+        <div class="cashback">
+          <span class="cashback__label">Saldo de cashback</span>
+          <span class="cashback__value">R$ 900,00</span>
         </div>
-        <div />
+        <div class="app-header__user-info">
+          <div
+            class="app-header__avatar"
+            style="background-image: url('https://cdn.icon-icons.com/icons2/1736/PNG/512/4043236-avatar-boy-male-portrait_113250.png')"
+          />
+          <div class="app-header__info">
+            <span class="app-header__welcome">Gustavo M.</span>
+            <button class="app-header__logout" title="Sair">Sair</button>
+          </div>
+        </div>
       </div>
     </div>
   </header>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,20 +1,27 @@
 <template>
   <nav class="nav-header">
-    <ul>
-      <li>
-        <router-link :to="{ name: 'dashboard' }" title="Suas Compras"
+    <ul class="nav-header__list">
+      <li class="nav-header__item">
+        <router-link
+          :to="{ name: 'dashboard' }"
+          title="Suas Compras"
+          class="nav-header__link"
           >Suas Compras</router-link
         >
       </li>
-      <li>
+      <li class="nav-header__item">
         <router-link
+          class="nav-header__link"
           :to="{ name: 'purchases-register' }"
           title="Cadastrar Compra"
           >Cadastrar Compra</router-link
         >
       </li>
-      <li>
-        <router-link :to="{ name: 'profile' }" title="Editar Perfil"
+      <li class="nav-header__item">
+        <router-link
+          :to="{ name: 'profile' }"
+          title="Editar Perfil"
+          class="nav-header__link"
           >Editar Perfil</router-link
         >
       </li>
