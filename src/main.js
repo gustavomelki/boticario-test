ecdeda2966c68cb69f02690fe5ce9dd9f4f1a90f
Gustavo Melki <contato@gustavomelki.com.br>
2019-11-30 21:48:00 -0200

    Separate plugins configs in own file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,12 @@
 import Vue from "vue";
-import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import money from "v-money";
-import Vuelidate from "vuelidate";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(fas);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
-Vue.use(money, { precision: 2 });
-Vue.use(Vuelidate);
+import "./plugins/axios";
+import "./plugins/fontawesome";
+import "./plugins/v-money";
+import "./plugins/vuelidate";
 
 Vue.config.productionTip = false;
 
