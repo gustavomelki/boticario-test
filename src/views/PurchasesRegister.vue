d715a8dd9b30c3e8b6d70e37fcbf8f5490103987
Gustavo Melki <contato@gustavomelki.com.br>
2019-11-30 18:03:23 -0200

    Add validations to input fields in Purchases page

--- src/views/PurchasesRegister.vue
+++ src/views/PurchasesRegister.vue
@@ -3,30 +3,82 @@
     <h1>Cadastro de Compras</h1>
     <p>Preencha os campos para efetuar o cadastro da sua compra</p>
     <div class="internal__card card card--white">
-      <form role="form" class="form-purchases-register">
-        <div class="input-group">
-          <input
-            type="text"
-            placeholder="Código da Compra"
-            class="form-purchases-register__input input-grey"
-          />
-          <input
-            type="tel"
-            placeholder="Data da Compra"
-            class="form-purchases-register__input input-grey"
-            v-mask="'##/##/####'"
-          />
+      <form
+        role="form"
+        ref="form"
+        class="form-purchases-register"
+        @submit.prevent="onSubmit"
+      >
+        <div class="input__group">
+          <div
+            class="form-purchases-register__input-container input__container"
+            :class="{ error: $v.codePurchase.$invalid && required }"
+          >
+            <input
+              class="form-purchases-register__input input--grey"
+              :class="{ error: $v.codePurchase.$error }"
+              v-model.trim="codePurchase"
+              type="text"
+              name="codePurchase"
+              value=""
+              placeholder="Código da Compra"
+            />
+            <span
+              class="error--message"
+              v-if="!$v.codePurchase.required && required"
+              >Obrigatório</span
+            >
+            <span class="error--message" v-if="!$v.codePurchase.minLength"
+              >Mínimo
+              {{ $v.codePurchase.$params.minLength.min }} caracteres</span
+            >
+          </div>
+          <div
+            class="form-purchases-register__input-container input__container"
+            :class="{ error: $v.datePurchase.$invalid && required }"
+          >
+            <input
+              class="form-purchases-register__input input--grey"
+              :class="{ error: $v.datePurchase.$error }"
+              v-model.trim="datePurchase"
+              type="tel"
+              name="datePurchase"
+              value=""
+              placeholder="Data da Compra"
+              v-mask="'##/##/####'"
+            />
+            <span
+              class="error--message"
+              v-if="!$v.datePurchase.required && required"
+              >Obrigatório</span
+            >
+            <span class="error--message" v-if="!$v.datePurchase.minLength"
+              >Data inválida</span
+            >
+          </div>
         </div>
-        <div class="input-group">
-          <input
-            type="tel"
-            placeholder="Valor da Compra"
-            class="form-purchases-register__input input-grey"
-            v-money="money"
-            v-model.lazy="price"
-          />
-          <div class="page-purchases-register__cashback">
-            <cashback label="Cashback gerado:" :value="cashback" />
+        <div class="input__group">
+          <div
+            class="form-purchases-register__input-container input__container"
+          >
+            <input
+              class="form-purchases-register__input input--grey"
+              v-model.lazy="valuePurchase"
+              type="tel"
+              name="valuePurchase"
+              value=""
+              placeholder="Valor da Compra"
+              v-money="money"
+            />
+          </div>
+          <div
+            class="form-purchases-register__input-container input__container"
+          >
+            <cashback
+              class="page-purchases-register__cashback"
+              label="Cashback gerado:"
+              :value="cashback"
+            />
           </div>
         </div>
         <button
@@ -43,6 +95,7 @@
 
 <script>
 import { mask } from "vue-the-mask";
+import { required, minLength } from "vuelidate/lib/validators";
 import Cashback from "@/components/Cashback";
 export default {
   name: "PurchasesRegister",
@@ -52,13 +105,16 @@ export default {
   directives: { mask },
   data() {
     return {
+      codePurchase: "",
       cashback: "0,00",
-      price: "",
+      datePurchase: "",
       money: {
         decimal: ",",
         thousands: ".",
         prefix: "R$ "
-      }
+      },
+      required: false,
+      valuePurchase: ""
     };
   },
   methods: {
@@ -79,11 +135,26 @@ export default {
       const number = num.toFixed(2).split(".");
       number[0] = `R$ ${number[0].split(/(?=(?:...)*$)/).join(".")}`;
       return number.join(",");
+    },
+    onSubmit() {
+      this.required = true;
+      if (this.$v.$invalid) return;
+      this.required = false;
     }
   },
   watch: {
-    price(value) {
+    valuePurchase(value) {
       this.calculateCashback(value);
+    }
+  },
+  validations: {
+    codePurchase: {
+      required,
+      minLength: minLength(5)
+    },
+    datePurchase: {
+      required,
+      minLength: minLength(10)
     }
   }
 };
