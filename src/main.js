3a02981a029d19b408bf22e5c9d39ffda408d1d1
Gustavo Melki <contato@gustavomelki.com.br>
2019-11-30 15:41:27 -0200

    Add vuelidate plugin

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 
 import money from "v-money";
+import Vuelidate from "vuelidate";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -13,6 +14,7 @@ library.add(fas);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.use(money, { precision: 2 });
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
