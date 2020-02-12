65e7e64c7ec65b7fa1eadebe5b7756f5dff23da3
Gustavo Melki <contato@gustavomelki.com.br>
2020-02-12 00:16:45 -0200

    adding some configs to tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: "@vue/cli-plugin-unit-jest",
+  verbose: true,
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/components/*.{js,vue}",
+    "src/views/*.{js,vue}",
+    "!**/node_modules/**"
+  ],
+  coverageReporters: ["html", "text-summary"]
 };
